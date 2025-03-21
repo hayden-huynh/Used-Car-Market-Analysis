bca8efa1ef1ab1792a886dd578de960f051d50b2
Hayden Huynh <huynhhai323@gmail.com>
2025-03-21 00:05:53 -0500

    Tested with new proxies. Better results yet still may crash. Handled with retry queue

--- data-extract/main.py
+++ data-extract/main.py
@@ -1,31 +1,53 @@
 from curl_cffi import requests
 from dotenv import load_dotenv
-from rich import print
-from rich.json import JSON
-import json
-from time import sleep
+import random
+import queue
 import os
 
 
 load_dotenv()
 proxy_key = os.getenv("SCRAPEOPS_API_KEY")
+smartproxy_endpoint = os.getenv("STICKY_SMARTPROXY")
 
-proxies = {
+scrapeops_proxies = {
     "http": f"http://scrapeops.country=us:{proxy_key}@residential-proxy.scrapeops.io:8181",
     "https": f"http://scrapeops.country=us:{proxy_key}@residential-proxy.scrapeops.io:8181",
 }
 
+smartproxy_proxies = {
+    "http": smartproxy_endpoint,
+    "https": smartproxy_endpoint,
+}
 
-def get_session():
-    session = requests.Session(impersonate="chrome", proxies=proxies)
+
+def send_curl_request(url):
+    browser = random.choice(["chrome", "edge", "safari", "firefox"])
+    resp = requests.get(
+        url,
+        impersonate=browser,
+        proxies=smartproxy_proxies,
+        verify=False,
+    )
+    return resp
+
+
+def get_curl_session():
+    browser = random.choice(["chrome", "edge", "safari", "firefox"])
+    session = requests.Session(
+        impersonate="chrome",
+        proxies=scrapeops_proxies,
+        verify=False,
+    )
     return session
 
 
-def request_listings_api(session: requests.Session, zip: str, distance: str, page: int):
+def request_listings_api(session, zip: str, distance: str, page: int):
     # Customizable parameters: zip, distance, pageNumber
     api_url = f"https://www.cargurus.com/Cars/searchPage.action?zip={zip}&distance={distance}&sourceContext=carGurusHomePageModel&inventorySearchWidgetType=AUTO&sortDir=ASC&sortType=BEST_MATCH&srpVariation=DEFAULT_SEARCH&isDeliveryEnabled=true&nonShippableBaseline=0&pageNumber={page}&filtersModified=true"
-    response = session.get(api_url)
-    response.raise_for_status()
+    try:
+        response = session.get(api_url)
+    except Exception as e:
+        print(f"Caught Error: {e}")
     data = response.json()
     car_ids = (
         [
@@ -39,84 +61,115 @@ def request_listings_api(session: requests.Session, zip: str, distance: str, pag
     return car_ids
 
 
-def request_details_api(session: requests.Session, id: str, zip: str, distance: str):
+def request_details_api(
+    session, id: str, zip: str, distance: str, retry_q: queue.Queue = None
+):
     # Customizable parameters: inventoryListing (car id), searchZip, searchDistance
     api_url = f"https://www.cargurus.com/Cars/detailListingJson.action?inventoryListing={id}&searchZip={zip}&searchDistance={distance}&inclusionType=DEFAULT&pid=null&sourceContext=carGurusHomePageModel&isDAVE=false"
-    response = session.get(api_url)
-    response.raise_for_status()
-    data = response.json()
-    info = data["listing"]
-    return {
-        "priceInfo": {
-            "price": info.get("price", ""),
-            "priceString": info.get("priceString", ""),
-            "dealRating": info.get("dealRatingKey", ""),
-            "savedCount": info.get("savedCount", ""),
-        },
-        "specs": {
-            "url": f"https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity=&zip={zip}#listing={id}",
+    try:
+        response = session.get(api_url)
+    except Exception as e:
+        print(f"Caught Error: {e}")
+        if retry_q is not None:
+            retry_q.put(id)
+            print(f"Added id {id} into retry queue")
+        return {}
+
+    if response.status_code == 200:
+        data = response.json()
+        info = data["listing"]
+        return {
             "vin": info.get("vin", ""),
-            "stockNumber": info.get("stockNumber", ""),
-            "make": info.get("makeName", ""),
-            "model": info.get("modelName", ""),
-            "year": info.get("year", ""),
-            "trimName": info.get("trimName", ""),
-            "mileage": info.get("mileage", ""),
-            "bodyType": (
-                data.get("autoEntityInfo").get("bodyStyle")
-                if "bodyStyle" in data.get("autoEntityInfo")
-                else ""
-            ),
-            "exteriorColor": info.get("localizedExteriorColor", ""),
-            "interiorColor": info.get("localizedInteriorColor", ""),
-            "driveTrain": info.get("localizedDriveTrain", ""),
-            "transmission": info.get("localizedTransmission", ""),
-            "mpgCity": (
-                info.get("cityFuelEconomy").get("value")
-                if "cityFuelEconomy" in info
-                else ""
-            ),
-            "mpgHighway": (
-                info.get("highwayFuelEconomy").get("value")
-                if "highwayFuelEconomy" in info
-                else ""
-            ),
-            "mpgCombined": (
-                info.get("combinedFuelEconomy").get("value")
-                if "combinedFuelEconomy" in info
-                else ""
-            ),
-            "fuelType": info.get("localizedFuelType", ""),
-            "options": info.get("options", ""),
-            "daysAtDealer": info.get("listingHistory").get("daysAtDealer", ""),
-            "daysOnCarGurus": info.get("listingHistory").get("daysOnCarGurus", ""),
-            "accidentCount": (
-                info.get("vehicleHistory").get("accidentCount")
-                if "accidentCount" in info.get("vehicleHistory")
-                else ""
-            ),
-            "ownerCount": (
-                info.get("vehicleHistory").get("ownerCount")
-                if "ownerCount" in info.get("vehicleHistory")
-                else ""
-            ),
-        },
-    }
+            "priceInfo": {
+                "price": info.get("price", ""),
+                "priceString": info.get("priceString", ""),
+                "dealRating": info.get("dealRatingKey", ""),
+                "savedCount": info.get("savedCount", ""),
+            },
+            "specs": {
+                "fullName": info.get("listingTitleOnly", ""),
+                "url": f"https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity=&zip={zip}#listing={id}",
+                "stockNumber": info.get("stockNumber", ""),
+                "make": info.get("makeName", ""),
+                "model": info.get("modelName", ""),
+                "year": info.get("year", ""),
+                "trimName": info.get("trimName", ""),
+                "mileage": info.get("mileage", ""),
+                "bodyType": (
+                    data.get("autoEntityInfo").get("bodyStyle")
+                    if "bodyStyle" in data.get("autoEntityInfo")
+                    else ""
+                ),
+                "exteriorColor": info.get("localizedExteriorColor", ""),
+                "interiorColor": info.get("localizedInteriorColor", ""),
+                "driveTrain": info.get("localizedDriveTrain", ""),
+                "transmission": info.get("localizedTransmission", ""),
+                "mpgCity": (
+                    info.get("cityFuelEconomy").get("value")
+                    if "cityFuelEconomy" in info
+                    else ""
+                ),
+                "mpgHighway": (
+                    info.get("highwayFuelEconomy").get("value")
+                    if "highwayFuelEconomy" in info
+                    else ""
+                ),
+                "mpgCombined": (
+                    info.get("combinedFuelEconomy").get("value")
+                    if "combinedFuelEconomy" in info
+                    else ""
+                ),
+                "fuelType": info.get("localizedFuelType", ""),
+                "options": info.get("options", ""),
+                "daysAtDealer": info.get("listingHistory").get("daysAtDealer", ""),
+                "daysOnCarGurus": info.get("listingHistory").get("daysOnCarGurus", ""),
+                "accidentCount": (
+                    info.get("vehicleHistory").get("accidentCount")
+                    if "accidentCount" in info.get("vehicleHistory")
+                    else ""
+                ),
+                "ownerCount": (
+                    info.get("vehicleHistory").get("ownerCount")
+                    if "ownerCount" in info.get("vehicleHistory")
+                    else ""
+                ),
+            },
+        }
+    else:
+        print(response.status_code)
+        return {}
 
 
 def main():
     zip = "75081"
     distance = "50"
     current_page = 1
-    session = get_session()
+    session = get_curl_session()
+    retry_queue = queue.Queue()
     while car_ids := request_listings_api(session, zip, distance, current_page):
+        # Extract data from all cars on current page
         print(
             f"=============================== Page {current_page} ====================================="
         )
         for id in car_ids:
-            car_data = request_details_api(session, id, zip, distance)
-            print(JSON(json.dumps(car_data)))
-            sleep(0.5)
+            car_data = request_details_api(session, id, zip, distance, retry_queue)
+            print("Car Not Found" if not car_data else car_data["specs"]["fullName"])
+            # sleep(random.uniform(0.2, 1.5))
+
+        # Retry failed car detail requests of the same page
+        if not retry_queue.empty():
+            print(
+                f"=============================== Page {current_page} Retries ====================================="
+            )
+            while not retry_queue.empty():
+                car_data = request_details_api(
+                    session, retry_queue.get(), zip, distance
+                )
+                print(
+                    "Car Not Found" if not car_data else car_data["specs"]["fullName"]
+                )
+
+        # Move to next page
         current_page += 1
 
 
